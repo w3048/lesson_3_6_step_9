ab463723e3fa366b5b2362f5e52ce09dd703082c
Eugen A. Korge <29kea02@gmail.com>
2021-03-11 10:23:34 +0300

    Add some comments to code, it's steel working succesfull

--- test_items.py
+++ test_items.py
@@ -5,8 +5,8 @@ def test_add_to_bascket(browser):
     '''ищем по уникальному вроде бы селектору, собираем все в список и в Ассерт
     проверяем, что в списке ровно один искомый элемент'''  
     browser.get(link)
-    time.sleep(30)
-    button_add_to_bascket = browser.find_elements_by_css_selector('.btn\
-                                  .btn-lg.btn-primary.btn-add-to-basket')                           
+    # 15 секунд вполне достаточно вроде, но оставим 30...
+    time.sleep(10)
+    # селектор .btn-add-to-basket уникальный, я проверил
+    button_add_to_bascket = browser.find_elements_by_css_selector('.btn-add-to-basket')                           
     assert len(button_add_to_bascket) == 1, "No button for Add to bascket"
-    
